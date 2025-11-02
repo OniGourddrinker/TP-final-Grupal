fd7442892814d1545af0229465c8bee28faf7327
OniGourddrinker <gonza_1086@hotmail.com>
2025-11-02 19:37:37 -0300

    trabajo que hizo fernan

---
+++ README.md
@@ -0,0 +1,10 @@
+Agregue movimiento en 4 dimensiones para el player
+Agregue al player a la escena principal
+En el ordering le puse 2 en Z index al player y la pelota
+Ordering +1 a los arcos
+Los player son layer 1, pelota layer 2, arco layer 3 y cancha layer 4
+Cambie la colision de la cancha, la anterior sigue estando pero no estaba funcionando, Creo que porque no tiene una forma cerrada (editado)domingo, 2 de noviembre de 2025 17:29
+
+Los dos chocan con el arco pero solo la pelota con la cancha, y el player no detecta la pelota
+
+Todo se agrego a la escena principal que es la cancha
--- Scripts/player_1.gd
+++ Scripts/player_1.gd
@@ -6,20 +6,25 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("patada") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("izquierda", "derecha")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	#Agregue direccion en y para que pueda moverse arriba y abajo copiando el codigo de derecha izquierda
+	#No recuerdo cual era la forma que lo hacia el profe
+	var directiony := Input.get_axis("arriba", "abajo")
+	if directiony:
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
